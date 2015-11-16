0aea86aac98ba351c1377685320035383f1f5655
Mathieu Brédif <mathieu.bredif@ign.fr>
2015-11-16 02:58:40 +0100

    extra strategy for simple aggregations (count and x,y,z/min,max) without filters for LAS relations

--- src/main/scala/fr/ign/spark/iqmulus/ExtraStrategies.scala
+++ src/main/scala/fr/ign/spark/iqmulus/ExtraStrategies.scala
@@ -21,18 +21,19 @@ import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.catalyst.InternalRow 
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.{Row, Strategy => SQLStrategy, SQLContext}
-import org.apache.spark.sql.catalyst.expressions.Count
 import org.apache.spark.sql.catalyst.expressions.{Alias, IntegerLiteral}
 //import org.apache.spark.sql.execution.datasources.LogicalRelation // private, see workaround below...
-import org.apache.spark.sql.types.LongType
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.catalyst.expressions.AttributeReference
+import org.apache.spark.sql.catalyst.expressions.{Min, Max, Count, Expression, NamedExpression}
 
+// optimized counts for PLY and LAS relations
 case class CountPlan(n: Long, sections: Array[BinarySection], name : String) extends SparkPlan {
-	
+  println("CountPlan optimization !")
   def count = n*sections.map(_.count).sum
   
   override def executeCollect(): Array[Row] = Array(Row(count))
-	
+  
   def doExecute() = sqlContext.sparkContext.parallelize(Seq(InternalRow(count)),1)
 
   def output: Seq[AttributeReference] = Seq(AttributeReference(name,LongType,nullable=false)())
@@ -40,32 +41,87 @@ case class CountPlan(n: Long, sections: Array[BinarySection], name : String) ext
   def children: Seq[SparkPlan] = Nil
 }
 
-object Strategy extends SQLStrategy with Serializable {
-	def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
-		// Find direct counts.
-	  case Aggregate(List(),List(Alias(Count(IntegerLiteral(lit)),name)),Project(_,child)) =>
-  	  //workaround private[sql] for org.apache.spark.sql.execution.datasources.LogicalRelation
-	    val field = child.getClass.getDeclaredFields.find(_.getName == "relation")
-	    field.foreach(_.setAccessible(true))
-	    field.map(_.get(child)) match {
-  	      case Some(r : BinarySectionRelation) => CountPlan(lit,r.sections,name) :: Nil
-	      case _ => Nil
-	    }
-	  case Aggregate(List(),List(Alias(Count(IntegerLiteral(lit)),name)),child) =>
-	    println("Filtered Count optimization ???")  
-	    println(child.getClass)  
-	    println(child)  
-        Nil
-	    
-	  case _ => 
-	    println(plan.getClass)  
-	    println(plan)  
-	    Nil // Return an empty list if we don't know how to handle this plan.
-	}
+// optimized aggregations for LAS relations (x/y/z)(min/xmax) and count
+case class AggregatePlan(aggregateExpressions: Seq[NamedExpression], headers: Array[las.LasHeader]) extends SparkPlan {
+  println("AggregatePlan optimization !")
+  
+  def get(expression: Expression) : Any = expression match {
+    case Alias(expr : Expression, _)        => get(expr)
+    case Min(AttributeReference("x",_,_,_)) => headers.map(h => (h.pmin(0)-h.offset(0))/h.scale(0)).min.toInt
+    case Max(AttributeReference("x",_,_,_)) => headers.map(h => (h.pmax(0)-h.offset(0))/h.scale(0)).max.toInt
+    case Min(AttributeReference("y",_,_,_)) => headers.map(h => (h.pmin(1)-h.offset(1))/h.scale(1)).min.toInt
+    case Max(AttributeReference("y",_,_,_)) => headers.map(h => (h.pmax(1)-h.offset(1))/h.scale(1)).max.toInt
+    case Min(AttributeReference("z",_,_,_)) => headers.map(h => (h.pmin(2)-h.offset(2))/h.scale(2)).min.toInt
+    case Max(AttributeReference("z",_,_,_)) => headers.map(h => (h.pmax(2)-h.offset(2))/h.scale(2)).max.toInt
+    case Count(IntegerLiteral(n))           => headers.map(_.pdr_nb ).sum * n
+  }
+  
+  override def executeCollect(): Array[Row] = Array(Row.fromSeq(aggregateExpressions.map(get _)))
+  
+  def doExecute() = sqlContext.sparkContext.parallelize(Seq(InternalRow.fromSeq(aggregateExpressions.map(get _))),1)
 
-	def register(bool : Boolean, sqlContext : SQLContext) = {
-		val strategies = sqlContext.experimental.extraStrategies.diff(Seq(Strategy))
-		sqlContext.experimental.extraStrategies = if (bool) Strategy +: strategies else strategies
-	}
+  def output: Seq[AttributeReference] = aggregateExpressions.map(a => AttributeReference(a.toAttribute.name,a.toAttribute.dataType)())
+
+  def children: Seq[SparkPlan] = Nil
+}
+
+object AggregatePlan {  
+  def handles(expression: Expression): Boolean = expression match {
+    case Alias(expr : Expression, _)    => AggregatePlan handles expr
+    case Min(attr : AttributeReference) => Seq("x","y","z") contains attr.name
+    case Max(attr : AttributeReference) => Seq("x","y","z") contains attr.name
+    case Count(_)                       => true
+    case _ => false
+  }
+}
+
+object Strategy extends SQLStrategy with Serializable {
+  
+  //work around private[sql] for org.apache.spark.sql.execution.datasources.LogicalRelation
+  def relationMap[Relation](plan : LogicalPlan, f : Relation => Seq[SparkPlan]) = {
+      val field = plan.getClass.getDeclaredFields.find(_.getName == "relation")
+      field.foreach(_.setAccessible(true))
+      field.map(_.get(plan)) match {
+        case Some(relation : Relation) => f(relation)
+        case _ => Nil
+      }
+  }
+  
+  def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
+      
+    case Aggregate(Nil,Seq(Alias(Count(IntegerLiteral(n)),name)),Project(_,logicalRelation)) =>
+      relationMap(logicalRelation,{ relation : BinarySectionRelation =>
+          new CountPlan(n,relation.sections,name) :: Nil
+      })
+
+    case Aggregate(Nil,agg,Project(_,logicalRelation)) =>
+      relationMap(logicalRelation,{ relation : las.LasRelation =>
+          if (!(agg forall (AggregatePlan handles _)))    Nil
+          else new AggregatePlan(agg,relation.headers) :: Nil
+      })
+
+      // debug
+    case Aggregate(exprs,aggs,child) =>
+      println("--> Unmatched Aggregate !")
+      println(exprs.getClass)  
+      println(exprs)  
+      println(aggs.getClass)  
+      println(aggs)  
+      println(child.getClass)  
+      println(child) 
+      Nil
+      
+      // debug
+    case _ => 
+      println("--> Unmatched!")
+      println(plan.getClass)  
+      println(plan)  
+      Nil
+  }
+
+  def register(bool : Boolean = true)(implicit sqlContext : SQLContext) = {
+    val strategies = sqlContext.experimental.extraStrategies.diff(Seq(Strategy))
+    sqlContext.experimental.extraStrategies = if (bool) Strategy +: strategies else strategies
+  }
 }
 
