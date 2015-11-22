03d9ff50b33e937d870d4838ae3dc47547ff0a80
Mathieu Brédif <mathieu.bredif@ign.fr>
2015-11-23 00:45:37 +0100

    rm commented code

--- src/main/scala/fr/ign/spark/iqmulus/las/LasOutputWriter.scala
+++ src/main/scala/fr/ign/spark/iqmulus/las/LasOutputWriter.scala
@@ -16,21 +16,6 @@
 
 package fr.ign.spark.iqmulus.las
 
-/*
-import java.io.{OutputStream, IOException}
-import java.nio.ByteBuffer
-import java.sql.Timestamp
-import java.util.HashMap
-
-import org.apache.hadoop.fs.Path
-import scala.collection.immutable.Map
-
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.io.NullWritable
-import org.apache.hadoop.mapreduce.{TaskAttemptID, RecordWriter, TaskAttemptContext}
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.sources.OutputWriter
- */
 import org.apache.spark.sql.types._
 import org.apache.hadoop.mapreduce.{ TaskAttemptID, RecordWriter, TaskAttemptContext }
 import java.io.DataOutputStream
@@ -43,38 +28,6 @@ import java.text.NumberFormat
 import org.apache.spark.sql.{ Row, SQLContext, sources }
 import fr.ign.spark.iqmulus.RowOutputStream
 
-/*
-class LasOutputFormat(outputFile: Path) extends FileOutputFormat[NullWritable, BytesWritable] {  
-  override def getDefaultWorkFile(context: TaskAttemptContext, extension: String): Path = {
-    val configuration = SparkHadoopUtil.get.getConfigurationFromJobContext(context)
-    val uniqueWriteJobId = configuration.get("spark.sql.sources.writeJobUUID")
-    val taskAttemptId = SparkHadoopUtil.get.getTaskAttemptIDFromTaskAttemptContext(context)
-    val split = taskAttemptId.getTaskID.getId
-    val name = FileOutputFormat.getOutputName(context)
-    new Path(outputFile, s"$name-$split%05d-$uniqueWriteJobId$extension")
-  }
-}
-
-class LasOutputWriter(path: String, dataSchema: StructType, context: TaskAttemptContext)
-  extends OutputWriter {
-
-  private val recordWriter: RecordWriter[NullWritable, BytesWritable] =
-    new LasOutputFormat(new Path(path)).getRecordWriter(context)
-
-  def toBytes(row: Row) : Array[Byte] = Array.empty[Byte]
-  val writable = new BytesWritable
-
-  override def write(row: Row): Unit = {
-    val bytes = toBytes(row)
-    writable.set(bytes,0,bytes.length)
-    recordWriter.write(null, writable)
-  }
-
-  override def close(): Unit = {
-    recordWriter.close(context)
-  }
-}
- */
 class LasOutputWriter(
   name: String,
   context: TaskAttemptContext,
@@ -138,109 +91,3 @@ class LasOutputWriter(
     dos.close
   }
 }
-
-/*
-// NOTE: This class is instantiated and used on executor side only, no need to be serializable.
-private[las] class LasOutputWriter(
-  path: String, dataSchema: StructType, context: TaskAttemptContext) 
-  extends OutputWriter {
-
-  /**
- * Overrides the couple of methods responsible for generating the output streams / files so
- * that the data can be correctly partitioned
- * */
-  /*
-  private val recordWriter: RecordWriter[NullWritable, BytesWritable] = {
-    val dos = new DataOutputStream(path)
-    LasHeader.schema(format).write(dos)
-
-  }
- */
-  private val format = 0
-
-  private val recordWriter: RecordWriter[NullWritable, BytesWritable] = {.getRecordWriter(context)
-  }
-
-
-  /*
-    new AvroKeyOutputFormat[GenericRecord]() {
-
-      private def getConfigurationFromContext(context: TaskAttemptContext): Configuration = {
-        // Use reflection to get the Configuration. This is necessary because TaskAttemptContext
-        // is a class in Hadoop 1.x and an interface in Hadoop 2.x.
-        val method = context.getClass.getMethod("getConfiguration")
-        method.invoke(context).asInstanceOf[Configuration]
-      }
-
-      override def getDefaultWorkFile(context: TaskAttemptContext, extension: String): Path = {
-        val uniqueWriteJobId =
-          getConfigurationFromContext(context).get("spark.sql.sources.writeJobUUID")
-        val taskAttemptId: TaskAttemptID = {
-          // Use reflection to get the TaskAttemptID. This is necessary because TaskAttemptContext
-          // is a class in Hadoop 1.x and an interface in Hadoop 2.x.
-          val method = context.getClass.getMethod("getTaskAttemptID")
-          method.invoke(context).asInstanceOf[TaskAttemptID]
-        }
-        val split = taskAttemptId.getTaskID.getId
-        new Path(path, f"part-r-$split%05d-$uniqueWriteJobId$extension")
-      }
-
-      @throws(classOf[IOException])
-      override def getAvroFileOutputStream(c: TaskAttemptContext): OutputStream = {
-        val path = getDefaultWorkFile(context, ".avro")
-        path.getFileSystem(getConfigurationFromContext(context)).create(path)
-      }
-
-    }.getRecordWriter(context)
- */
-  override def write(row: Row): Unit = recordWriter write row
-
-  override def close(): Unit = recordWriter close
-
-}
- */
-
-/*
-
-package fr.ign.spark.iqmulus
-
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.Column
-import java.io.DataOutputStream
-import org.apache.hadoop.fs.Path
-import org.apache.hadoop.conf.Configuration
-
-package object las {
-
-  implicit class LASDataFrame(df: DataFrame) {
-    val defaultAliases : Map[String,String] = Map[String,String]()
-    def saveAsLasFile(location: String, format : Byte,
-      aliases : Map[String,String] = defaultAliases) = {
-      val names = aliases
-      val schema = LASHeader.schema(format) // no user types for now
-      val cols   = schema.fieldNames.intersect(df.schema.fieldNames)
-      val partitionSize = df.rdd.mapPartitionsWithIndex { case (pid, iter) =>
-        Iterator((pid, iter.size)) }.collect.toMap
-      df.select(cols.head, cols.tail :_*).rdd.mapPartitionsWithIndex({
-        case (pid, iter) =>
-          val name = s"$location/$pid.las"
-          val path = new Path(name)
-          val fs = path.getFileSystem(new Configuration)
-          val f = fs.create(path)
-          val count = partitionSize(pid)
-          val header = new LASHeader(name,count,format)
-          val dos = new DataOutputStream(f);
-          header.write(dos)
-          iter.foreach(dos write _)
-          dos.close
-          Iterator((path.toString, count))
-      }, true)
-    }
-
-  }
-
-}
-
- */
-
