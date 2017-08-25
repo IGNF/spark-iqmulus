d7df908d12888335cce459c8a82b7a0f62455c31
lcaraffa <caraffa.laurent@gmail.com>
2017-08-25 19:41:34 +0200

    xyz works in spark 2.2 !

--- src/main/scala/fr/ign/spark/iqmulus/xyz/DefaultSource.scala
+++ src/main/scala/fr/ign/spark/iqmulus/xyz/DefaultSource.scala
@@ -1,5 +1,6 @@
+
 /*
- * Copyright 2015 IGN
+ * Copyright 2015-2017 IGN
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,20 +17,155 @@
 
 package fr.ign.spark.iqmulus.xyz
 
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.execution.datasources.HadoopFsRelation // HadoopFsRelationProvider
-import org.apache.spark.sql.types.StructType
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.io.IOException
+import scala.util.control.NonFatal
+import org.apache.hadoop.fs.FileStatus
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.TaskAttemptContext
+import org.apache.spark.TaskContext
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.execution.datasources.TextBasedFileFormat
+import org.apache.spark.sql.execution.datasources.OutputWriter
+import org.apache.spark.sql.execution.datasources.PartitionedFile
+import org.apache.spark.sql.execution.datasources.OutputWriterFactory
+import org.apache.spark.sql.execution.datasources.HadoopFileLinesReader
+import org.apache.spark.sql.sources.DataSourceRegister
+import org.apache.spark.sql.sources.Filter
+import org.apache.spark.sql.types._
 
-class DefaultSource extends HadoopFsRelationProvider {
+class SerializableConfiguration(@transient var value: Configuration) extends Serializable {
+  private def tryOrIOException[T](block: => T): T = {
+    try {
+      block
+    } catch {
+      case e: IOException =>
+        throw e
+      case NonFatal(e) =>
+        throw new IOException(e)
+    }
+  }
 
-  // override def shortName(): String = "xyz"
+  private def writeObject(out: ObjectOutputStream): Unit = tryOrIOException {
+    out.defaultWriteObject()
+    value.write(out)
+  }
 
-  override def createRelation(
-    sqlContext: SQLContext,
-    paths: Array[String],
-    dataSchema: Option[StructType],
-    partitionColumns: Option[StructType],
-    parameters: Map[String, String]): HadoopFsRelation = {
-    new XyzRelation(paths, dataSchema, partitionColumns, parameters)(sqlContext)
+  private def readObject(in: ObjectInputStream): Unit = tryOrIOException {
+    value = new Configuration(false)
+    value.readFields(in)
   }
 }
+
+object XyzFileFormat {
+
+  val xyzrgbSchema = StructType(Array(
+    StructField("x", FloatType, false),
+    StructField("y", FloatType, false),
+    StructField("z", FloatType, false),
+    StructField("r", ByteType, false),
+    StructField("g", ByteType, false),
+    StructField("b", ByteType, false)))
+
+  val xyzSchema = StructType(Array(
+    StructField("x", FloatType, false),
+    StructField("y", FloatType, false),
+    StructField("z", FloatType, false)))
+
+}
+
+/**
+ * Provides access to XYZ data from pure SQL statements.
+ */
+class DefaultSource extends TextBasedFileFormat with DataSourceRegister {
+
+  override def shortName(): String = "xyz"
+
+  override def toString: String = "XYZ"
+
+  override def hashCode(): Int = getClass.hashCode()
+
+  override def equals(other: Any): Boolean = other.isInstanceOf[DefaultSource]
+
+  override def inferSchema(
+    sparkSession: SparkSession,
+    options: Map[String, String],
+    files: Seq[FileStatus]): Option[StructType] = options.getOrElse("format", "xyz") match {
+    case "xyzrgb" => Some(XyzFileFormat.xyzrgbSchema)
+    case "xyz" => Some(XyzFileFormat.xyzSchema)
+    case _ => None
+  }
+
+  override def prepareWrite(
+    sparkSession: SparkSession,
+    job: Job,
+    options: Map[String, String],
+    dataSchema: StructType): OutputWriterFactory = {
+    new OutputWriterFactory {
+      override def newInstance(
+        path: String,
+        dataSchema: StructType,
+        context: TaskAttemptContext): OutputWriter = {
+        new XyzOutputWriter(path, dataSchema, context)
+      }
+      override def getFileExtension(context: TaskAttemptContext): String = ".xyz"
+    }
+  }
+
+  override def buildReader(
+    sparkSession: SparkSession,
+    dataSchema: StructType,
+    partitionSchema: StructType,
+    requiredSchema: StructType,
+    filters: Seq[Filter],
+    options: Map[String, String],
+    hadoopConf: Configuration): (PartitionedFile) => Iterator[InternalRow] = {
+
+    val broadcastedHadoopConf =
+      sparkSession.sparkContext.broadcast(new SerializableConfiguration(hadoopConf))
+
+    (file: PartitionedFile) => {
+      val conf = broadcastedHadoopConf.value.value
+      val linesReader = new HadoopFileLinesReader(file, conf)
+      Option(TaskContext.get()).foreach(_.addTaskCompletionListener(_ => linesReader.close()))
+      val dataTypes = dataSchema.fields.map(_.dataType)
+      linesReader.map { line =>
+        InternalRow.fromSeq((line.toString.trim.split("\t") zip dataTypes).map {
+          case (x, StringType) => x
+          case (x, ByteType) => x.toByte
+          case (x, ShortType) => x.toShort
+          case (x, IntegerType) => x.toInt
+          case (x, LongType) => x.toLong
+          case (x, FloatType) => x.toFloat
+          case (x, DoubleType) => x.toDouble
+          case _ => null
+        }.padTo(dataTypes.size, null))
+      }
+    }
+  }
+
+}
+
+private[xyz] class XyzOutputWriter(
+  filename: String,
+  dataSchema: StructType,
+  context: TaskAttemptContext)
+  extends OutputWriter {
+
+  private lazy val dos = {
+    val path = new org.apache.hadoop.fs.Path(filename)
+    val fs = path.getFileSystem(context.getConfiguration)
+    val f = fs.create(path)
+    new java.io.DataOutputStream(f)
+  }
+
+  override def write(row: InternalRow): Unit = {
+    dos.writeBytes(row.toSeq(dataSchema).mkString("", "\t", "\n"))
+  }
+
+  override def close(): Unit = dos.close
+}
+
--- src/main/scala/fr/ign/spark/iqmulus/xyz/package.scala
+++ src/main/scala/fr/ign/spark/iqmulus/xyz/package.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 IGN
+ * Copyright 2015-2017 IGN
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,9 @@
 
 package fr.ign.spark.iqmulus
 
-import org.apache.spark.sql.{ SQLContext, DataFrameReader, DataFrameWriter, DataFrame, Row }
-import org.apache.spark.sql.types.{ FloatType, StructType }
+import org.apache.spark.sql.DataFrameReader
+import org.apache.spark.sql.DataFrameWriter
+import org.apache.spark.sql.DataFrame
 
 package object xyz {
 
@@ -25,39 +26,16 @@ package object xyz {
    * Adds a method, `xyz`, to DataFrameWriter that allows you to write xyz files using
    * the DataFileWriter
    */
-  implicit class XyzDataFrameWriter(writer: DataFrameWriter) {
+  implicit class XyzDataFrameWriter[T](writer: DataFrameWriter[T]) {
     def xyz: String => Unit = writer.format("fr.ign.spark.iqmulus.xyz").save
   }
 
   /**
    * Adds a method, `xyz`, to DataFrameReader that allows you to read xyz files using
-   * the DataFileReade
+   * the DataFileReader
    */
   implicit class XyzDataFrameReader(reader: DataFrameReader) {
     def xyz: String => DataFrame = reader.format("fr.ign.spark.iqmulus.xyz").load
   }
 
-  implicit class XyzDataFrame(df: DataFrame) {
-    def saveAsXyz(location: String) = {
-      val df_id = df.drop("id")
-      require(df_id.schema.fieldNames.take(3) sameElements Array("x", "y", "z"))
-      require(df_id.schema.fields.map(_.dataType).take(3).forall(_ == FloatType))
-      val saver = (key: Int, iter: Iterator[Row]) => Iterator(iter.saveXyz(s"$location/$key.xyz"))
-      df_id.rdd.mapPartitionsWithIndex(saver, true).collect
-    }
-  }
-
-  implicit class XyzRowIterator(iter: Iterator[Row]) {
-    def saveXyz(filename: String) = {
-      val path = new org.apache.hadoop.fs.Path(filename)
-      val fs = path.getFileSystem(new org.apache.hadoop.conf.Configuration)
-      val f = fs.create(path)
-      val dos = new java.io.DataOutputStream(f)
-      var count = 0L
-      iter.foreach(row => { count += 1; dos.writeBytes(row.mkString("", "\t", "\n")) })
-      dos.close
-      (filename, count)
-    }
-  }
 }
-
